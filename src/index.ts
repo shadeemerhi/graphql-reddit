36e1929d4a952ffd50eb5acd9596499640e23b51
Shadee Merhi <shadmerhi@gmail.com>
2022-01-08 22:59:00 -0700

    install and configure graphql

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
     await orm.getMigrator().up();
+    const app = express();
 
-    const post = orm.em.create(Post, { title: "My First Post" });
-    await orm.em.persistAndFlush(post);
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false,
+        }),
+    });
 
-    const posts = await orm.em.find(Post, {});
-    console.log('HERE ARE THE POSTS', posts);
-    
+    // Creates graphql endpoint
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log("Server Started on PORT 4000");
+    });
 };
 
 main().catch((err) => {
