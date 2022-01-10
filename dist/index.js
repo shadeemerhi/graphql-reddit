f8cc8634509cceecd62e4cf7fdf152389320b066
Shadee Merhi <shadmerhi@gmail.com>
2022-01-10 10:40:50 -0700

    add register/login validations

--- dist/index.js
+++ dist/index.js
@@ -10,13 +10,14 @@ const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
 const type_graphql_1 = require("type-graphql");
 const post_1 = require("./resolvers/post");
+const user_1 = require("./resolvers/user");
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
     await orm.getMigrator().up();
     const app = (0, express_1.default)();
     const apolloServer = new apollo_server_express_1.ApolloServer({
         schema: await (0, type_graphql_1.buildSchema)({
-            resolvers: [post_1.PostResolver],
+            resolvers: [user_1.UserResolver, post_1.PostResolver],
             validate: false,
         }),
         context: () => ({ em: orm.em }),
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,11 +1,14 @@
-import { User } from "src/entities/User";
+import { User } from "../entities/User";
 import { MyContext } from "src/types";
+import argon2 from "argon2";
+
 import {
     Arg,
     Ctx,
     Field,
     InputType,
     Mutation,
+    ObjectType,
     Query,
     Resolver,
 } from "type-graphql";
@@ -19,14 +22,120 @@ class UsernamePasswordInput {
     password: string;
 }
 
+@ObjectType()
+class UserResponse {
+    @Field(() => User, { nullable: true })
+    user?: User;
+
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[];
+}
+
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+
+    @Field()
+    message: string;
+}
+
 @Resolver()
 export class UserResolver {
-    @Mutation(() => String)
+    @Query(() => [User])
+    async findUsers(@Ctx() { em }: MyContext): Promise<User[]> {
+        return await em.find(User, {});
+    }
+
+    @Mutation(() => UserResponse)
     async register(
         @Arg("options") options: UsernamePasswordInput,
         @Ctx() { em }: MyContext
-    ) {
-        const user = em.create(User, { username: options.username });
-        await em.persistAndFlush(user);
+    ): Promise<UserResponse> {
+        if (options.username.length <= 2) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "Username must be greater than 2 characters",
+                    },
+                ],
+            };
+        }
+
+        if (options.password.length <= 2) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "Password must be at least 2 characters",
+                    },
+                ],
+            };
+        }
+        const hashedPassword = await argon2.hash(options.password);
+        const user = em.create(User, {
+            username: options.username,
+            password: hashedPassword,
+        });
+
+        try {
+            await em.persistAndFlush(user);
+        } catch (error) {
+            if (
+                error.code === "23505" ||
+                error.detail.includes("already exists")
+            ) {
+                // throw new Error("Username has already been used"); // Why not just do this?
+                return {
+                    errors: [
+                        {
+                            field: "username",
+                            message: "Username has already been used",
+                        },
+                    ],
+                };
+            }
+            console.log(error);
+        }
+        return {
+            user,
+        };
+    }
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg("options") options: UsernamePasswordInput,
+        @Ctx() { em }: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, { username: options.username });
+
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "Username does not exist",
+                    },
+                ],
+            };
+        }
+
+        const valid = await argon2.verify(user.password, options.password);
+
+        if (!valid) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "Incorrect password",
+                    },
+                ],
+            };
+        }
+
+        return {
+            user,
+        };
     }
 }
