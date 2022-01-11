2dfdc9674bade1d023cad4d1990d96326b881fe4
Shadee Merhi <shadmerhi@gmail.com>
2022-01-10 22:20:14 -0700

    add register form

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,12 +1,6 @@
-// import React from "react";
-import {
-    FormControl,
-    FormLabel,
-    Input,
-    FormErrorMessage,
-    Button,
-} from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
+import InputField from "../components/InputField";
 import Wrapper from "../components/Wrapper";
 
 interface RegisterProps {}
@@ -20,18 +14,30 @@ const Register: React.FC<RegisterProps> = ({}) => {
                     console.log(values);
                 }}
             >
-                {({ values, handleChange }) => (
+                {({ isSubmitting }) => (
                     <Form>
-                        <FormControl>
-                            <FormLabel htmlFor="username">Username</FormLabel>
-                            <Input
-                                value={values.username}
-                                onChange={handleChange}
-                                id="username"
-                                placeholder="username"
+                        <InputField
+                            name="username"
+                            placeholder="username"
+                            label="Username"
+                        />
+                        <Box mt={4}>
+                            <InputField
+                                name="password"
+                                placeholder="password"
+                                label="Password"
+                                type="password"
                             />
-                            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-                        </FormControl>
+                        </Box>
+                        <Button
+                            type="submit"
+                            mt={4}
+                            colorScheme="teal"
+                            isLoading={isSubmitting}
+                            color="white"
+                        >
+                            Register
+                        </Button>
                     </Form>
                 )}
             </Formik>
