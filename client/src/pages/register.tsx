82a5a5c7be5a6fd0e44585abf012b498fed42333
Shadee Merhi <shadmerhi@gmail.com>
2022-01-12 20:01:29 -0700

    add urql wrapper and register mutation

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -3,15 +3,36 @@ import { Formik, Form } from "formik";
 import InputField from "../components/InputField";
 import Wrapper from "../components/Wrapper";
 
+import { useMutation } from "urql";
+
 interface RegisterProps {}
 
+const REGISTER_MUTATION = `mutation Register($username: String!, $password: String!) {
+  register(options: {username: $username, password: $password}) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}`;
+
 const Register: React.FC<RegisterProps> = ({}) => {
+    const [{ data, fetching, error }, register] =
+        useMutation(REGISTER_MUTATION);
+
+    console.log("HERE ARE THINGS", data, fetching, error);
+
     return (
         <Wrapper>
             <Formik
                 initialValues={{ username: "", password: "" }}
                 onSubmit={(values) => {
                     console.log(values);
+                    return register(values);
                 }}
             >
                 {({ isSubmitting }) => (
