46a8bda510eb07bd9787badc387f50e3f2b58a3f
Shadee Merhi <shadmerhi@gmail.com>
2022-01-12 21:36:07 -0700

    create login page and mutation

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,76 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import InputField from "../components/InputField";
+import Wrapper from "../components/Wrapper";
+
+import { useMutation } from "urql";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+
+interface LoginProps {}
+
+const REGISTER_MUTATION = `mutation Register($username: String!, $password: String!) {
+  register(options: {username: $username, password: $password}) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}`;
+
+const Login: React.FC<LoginProps> = ({}) => {
+    const router = useRouter();
+    const [{ data, fetching, error }, login] = useLoginMutation();
+
+    return (
+        <Wrapper>
+            <Formik
+                initialValues={{ username: "", password: "" }}
+                onSubmit={async (values, { setErrors }) => {
+                    console.log(values);
+                    const response = await login({ options: values });
+                    if (response.data?.login.errors) {
+                        setErrors(toErrorMap(response.data.login.errors));
+                    } else if (response.data?.login.user) {
+                        // worked
+                        router.push("/");
+                    }
+                }}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <InputField
+                            name="username"
+                            placeholder="username"
+                            label="Username"
+                        />
+                        <Box mt={4}>
+                            <InputField
+                                name="password"
+                                placeholder="password"
+                                label="Password"
+                                type="password"
+                            />
+                        </Box>
+                        <Button
+                            type="submit"
+                            mt={4}
+                            colorScheme="teal"
+                            isLoading={isSubmitting}
+                            color="white"
+                        >
+                            Login
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
+    );
+};
+
+export default Login;
