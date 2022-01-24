bf9920ae6493ee90912fb415180188fe9141b317
Shadee Merhi <shadmerhi@gmail.com>
2022-01-24 13:34:28 -0700

    update deploy script

--- server/deploy.sh
+++ server/deploy.sh
@@ -6,4 +6,4 @@ echo $VERSION
 
 docker build -t shadeemerhi/graphreddit:$VERSION .
 docker push shadeemerhi/graphreddit:$VERSION
-ssh root@159.223.180.68 "docker pull shadeemerhi/graphreddit:$VERSION && docker tag shadeemerhi/graphreddit:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
+ssh root@159.223.180.68 "docker pull shadeemerhi/graphreddit:$VERSION && docker tag shadeemerhi/graphreddit:$VERSION dokku/api:$VERSION && dokku tags:deploy api $VERSION"
